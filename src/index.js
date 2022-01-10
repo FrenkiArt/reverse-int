68ffbd4da54460e9533a303b928e4faf6b122ad1
frenkiart <frenki_art@mail.ru>
2022-01-11 02:14:16 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let strN;
+
+    if (n < 0) {
+        strN = String(n * -1);
+        return Number(strN.split("").reverse().join(""));
+    } else {
+        strN = String(n);
+        return Number(strN.split("").reverse().join(""));
+    }
+};
